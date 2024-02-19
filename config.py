b24d43654c0552250cea9253e9b90ef98cb83397
Santoyo18 <santoyoalejandro262@gmail.com>
2024-02-19 08:38:14 -0500

    XD

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 class Config:
     # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/Registrodecomputadoras"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:hCdBGEd4cCAd3CbgFggGghAefgE6f41-@roundhouse.proxy.rlwy.net:45538/Registrodecomputadoras"
+    #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:hCdBGEd4cCAd3CbgFggGghAefgE6f41-@roundhouse.proxy.rlwy.net:45538/Registrodecomputadoras"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:a5ba4g6C1bBG2Gec6dBBcHg6bCgbDF44@monorail.proxy.rlwy.net:16262/Registrodecomputadoras"
+
     SQLALCHEMY_TRACK_MODIFICATIONS = True
  
